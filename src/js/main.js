dbad3688e13405a1a48565f312bed48cb1a3409e
razuvaevcr <razuvaj@gmail.com>
2022-01-20 13:12:05 +0300

    add modal behavior

--- src/js/main.js
+++ src/js/main.js
@@ -91,5 +91,57 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline);
+
+    //Modal
+
+    const modalTriggers = document.querySelectorAll('[data-modal]'),
+        modalCloseBtn = document.querySelector('[data-close]'),
+        modal = document.querySelector('.modal');
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden'; //отменяем прокрутку страницы
+        clearInterval(modalTimerID);
+    }
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = ''; //возвращаем прокрутку   
+    }
+
+
+    modalTriggers.forEach((trigger) => {
+        trigger.addEventListener('click', openModal);
+    });
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }    
+    });
+
+    document.addEventListener('keydown', (e) => { //
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    const modalTimerID = setTimeout(openModal, 2000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.
+            documentElement.scrollHeight - 1) { //пользователь долистал до конца
+            openModal();
+
+            clearInterval(modalTimerID);//чтобы не повторялось открытие при увеличении времени
+            window.removeEventListener('scroll', showModalByScroll);//удаляем, чтобы не повторялась
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
 
